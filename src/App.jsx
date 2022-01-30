4706e47848aae696533ad1043f208c0df6e47107
Jason Telfer <jason@jasont.dev>
2022-01-30 21:10:28 +1000

    Removed conflicting configs from eslintrc

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { ApolloClient, ApolloProvider } from '@apollo/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
+
 import Login from './routes/Login';
 
-function App() {
+function App({ apolloClient }) {
   return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-    </Routes>
+    <ApolloProvider client={apolloClient}>
+      <BrowserRouter>
+        <ChakraProvider>
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </ChakraProvider>
+      </BrowserRouter>
+    </ApolloProvider>
   );
 }
+
+App.propTypes = {
+  apolloClient: PropTypes.instanceOf(ApolloClient).isRequired
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,35 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ChakraProvider } from '@chakra-ui/react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import Login from './routes/Login';
 
 const client = new ApolloClient({
-  uri: process.env.BACKEND,
+  uri: process.env.REACT_APP_BACKEND,
   cache: new InMemoryCache()
 });
 
 ReactDOM.render(
-  <React.StrictMode>
-    <ApolloProvider client={client}>
-      <BrowserRouter>
-        <ChakraProvider>
-          <Routes>
-            <Route path="/" element={<App />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
-        </ChakraProvider>
-      </BrowserRouter>
-    </ApolloProvider>
-  </React.StrictMode>,
+  React.createElement(App, { apolloClient: client }, null),
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
