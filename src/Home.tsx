f986536ac2abcc49225959a734a2e534411ab12e
Jason T <jason@jasont.dev>
2022-04-15 20:23:31 +1000

    Implemented user query in Home to fetch name and id

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,9 +2,30 @@ import React, { useEffect } from "react";
 import Navbar from "./Navbar";
 import { CenteredContent } from "./layout";
 import { useAuth } from "./auth";
+import { useQuery } from "react-query";
 
 export default function Home() {
+  const readUser = async () => {
+    return fetch("http://localhost:4100/graphql", {
+      method: "POST",
+      headers: {
+        Authorization: "Bearer " + auth.getAccessToken(),
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: `
+      query User {
+        user {
+          id
+          name
+        }
+      }
+      `,
+      }),
+    }).then((res) => res.json());
+  };
   const auth = useAuth();
+  const { data } = useQuery("user", readUser);
 
   useEffect(() => {
     if (!auth.getAccessToken()) {
@@ -16,7 +37,11 @@ export default function Home() {
     <CenteredContent>
       <Navbar />
       <h1>Tasklists</h1>
-      <h3>Hello. Welcome to Mutodo.</h3>
+      {data ? (
+        <h3>Hello, {data.data.user.name}, and welcome to Mutodo.</h3>
+      ) : (
+        <h3>Hello. Welcome to Mutodo.</h3>
+      )}
     </CenteredContent>
   );
 }
