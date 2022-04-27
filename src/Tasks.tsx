99dea9685f78db09cb22e7aa1b383030addb8b60
Jason Telfer <jason@jasont.dev>
2022-04-27 21:24:12 +1000

    Removed some unwanted files

--- src/Tasks.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { Layout } from "./layout";
-import Navbar from "./Navbar";
-
-export default function Tasklist() {
-  return (<Layout><Navbar title="Tasklist" /></Layout>);
-}
